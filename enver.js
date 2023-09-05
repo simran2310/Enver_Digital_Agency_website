1f014dd7915c26a374c3ed00032266247e573ca2
simran2310 <111453166+simran2310@users.noreply.github.com>
2023-09-05 12:11:58 +0530

    Add files via upload

---
+++ enver.js
@@ -0,0 +1,7 @@
+function myMenu() {
+    var togggleBtn = document.getElementById("menuBar");
+    togggleBtn.classList.toggle("show")
+}
+
+// const mobile_menu = document.querySelector("mobile-menu");
+// mobile_menu.addEventListener('click', () => toggleNavbar());
